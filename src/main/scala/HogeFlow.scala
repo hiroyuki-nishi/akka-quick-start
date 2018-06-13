2d5c52a008501de71e87ce6434847cfc77dd5b06
hiroyuki.nishi <hiroyuki.nishi@motex.co.jp>
2018-06-13 23:56:04 +0900

    custome stream作成

--- src/main/scala/HogeFlow.scala
+++ src/main/scala/HogeFlow.scala
@@ -24,6 +24,7 @@ class DynamoDbFlow {
   implicit val ec: ExecutionContextExecutor = system.dispatcher
 
   val source: Source[Int, NotUsed] = Source(1 to 100)
+  lazy val customeFlow: Flow[Int, Int, NotUsed] = Flow.fromGraph(new CustomStream())
   //TODO -nishi Future[Done]って何？
   val sink1: Sink[Int, Future[Done]] = Sink.foreach[Int](x => println(x))
   val sink2: Sink[Int, Future[Done]] = Sink.foreach[Int](x => println(x))
@@ -32,10 +33,37 @@ class DynamoDbFlow {
 
     val broadcast = b.add(Broadcast[Int](2))
     source ~> broadcast.in
-    broadcast.out(0) ~> Flow[Int].map(x => x) ~> sink1
+//    broadcast.out(0) ~> Flow[Int].map(x => x) ~> sink1
+    broadcast.out(0) ~> customeFlow ~> sink1
     broadcast.out(1) ~> Flow[Int].map(x => x) ~> sink1
     ClosedShape
   })
 
   def run(): Unit = g.run()
+}
+
+import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+
+class CustomStream extends GraphStage[FlowShape[Int, Int]] {
+  val in = Inlet[Int]("CostomStream.in")
+  val out = Outlet[Int]("CostomStream.out")
+
+  override val shape = FlowShape.of(in, out)
+
+  override def createLogic(attr: Attributes): GraphStageLogic =
+    new GraphStageLogic(shape) {
+      setHandler(in, new InHandler {
+        override def onPush(): Unit = {
+          val hoge = grab(in)
+          println(in)
+          push(out, hoge)
+        }
+      })
+      setHandler(out, new OutHandler {
+        override def onPull(): Unit = {
+          pull(in)
+        }
+      })
+    }
 }
